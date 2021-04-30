254292ec773f91cd4fe0899804db2b2742446aef
Nate Sales <nate@natesales.net>
2021-04-30 12:09:15 -0700

    add tests

--- main_test.go
+++ main_test.go
@@ -5,8 +5,79 @@ import (
 	"testing"
 )
 
-func TestUDPQuery(t *testing.T) {
-	if err := driver(strings.Split("example.com @1.1.1.1", " ")); err != nil {
+func clearOpts() {
+	opts.Name = ""
+	opts.Server = ""
+	opts.Types = []string{}
+	opts.Reverse = false
+	opts.DNSSEC = false
+	opts.Format = "pretty"
+	opts.Chaos = false
+	opts.OdohProxy = ""
+	opts.Insecure = false
+	opts.Timeout = 10
+	opts.Verbose = false
+}
+
+func TestCLIUDPQuery(t *testing.T) {
+	clearOpts()
+	if err := driver([]string{
+		"example.com",
+	}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCLIODOHQuery(t *testing.T) {
+	clearOpts()
+	if err := driver([]string{
+		"-v",
+		"-q", "example.com",
+		"-s", "https://odoh.cloudflare-dns.com",
+		"--odoh-proxy", "https://odoh1.surfdomeinen.nl",
+	}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRawFormat(t *testing.T) {
+	clearOpts()
+	if err := driver([]string{
+		"example.com",
+		"--format=raw",
+	}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestJSONFormat(t *testing.T) {
+	clearOpts()
+	if err := driver([]string{
+		"example.com",
+		"--format=json",
+	}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInvalidFormat(t *testing.T) {
+	clearOpts()
+	err := driver([]string{
+		"example.com",
+		"--format=invalid",
+	})
+	if !(err != nil && strings.Contains(err.Error(), "invalid output format")) {
+		t.Errorf("invalid output format should throw an error")
+	}
+}
+
+func TestParseTypes(t *testing.T) {
+	clearOpts()
+	if err := driver([]string{
+		"example.com",
+		"A",
+		"AAAA",
+	}); err != nil {
 		t.Error(err)
 	}
 }
--- resolver_test.go
+++ resolver_test.go
@@ -16,7 +16,32 @@ func TestResolveUDP(t *testing.T) {
 		t.Error(err)
 	}
 
-	answers, qTime, err := Resolve("example.com", false, "", u, []uint16{dns.StringToType["A"]})
+	answers, qTime, err := resolve("example.com", false, false, "", u, []uint16{dns.StringToType["A"]})
+	if err != nil {
+		t.Error(err)
+	}
+
+	queryTime := uint16(qTime / time.Millisecond) // Convert to milliseconds
+
+	if len(answers) < 1 {
+		t.Errorf("expected more than 1 answer, got %d", len(answers))
+	}
+
+	if queryTime > 1000 {
+		t.Errorf("query took longer than 1 second, %d ms", queryTime)
+	}
+}
+
+func TestDNSSECResolveUDP(t *testing.T) {
+	u, err := upstream.AddressToUpstream("1.1.1.1:53", upstream.Options{
+		Timeout:            10 * time.Second,
+		InsecureSkipVerify: opts.Insecure,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	answers, qTime, err := resolve("example.com", false, true, "", u, []uint16{dns.StringToType["A"]})
 	if err != nil {
 		t.Error(err)
 	}
@@ -41,7 +66,7 @@ func TestResolveODOH(t *testing.T) {
 		t.Error(err)
 	}
 
-	answers, qTime, err := Resolve("example.com", false, "odoh1.surfdomeinen.nl", u, []uint16{dns.StringToType["A"]})
+	answers, qTime, err := resolve("example.com", false, false, "odoh1.surfdomeinen.nl", u, []uint16{dns.StringToType["A"]})
 	if err != nil {
 		t.Error(err)
 	}
