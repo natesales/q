133c3f82ce41cc2121f441f02cbaddafff4798e8
Nate Sales <nate@natesales.net>
2022-04-23 22:03:29 -0700

    fix: reject plus flags as qname

--- main.go
+++ main.go
@@ -319,7 +319,12 @@ All long form (--) flags can be toggled with the dig-standard +[no]flag notation
 		}
 
 		// Set qname if not set by flag
-		if opts.Name == "" && (strings.Contains(arg, ".") || strings.Contains(arg, ":")) && !strings.Contains(arg, "@") && !strings.Contains(arg, "/") && !strings.HasPrefix(arg, "-") {
+		if opts.Name == "" &&
+			(strings.Contains(arg, ".") || strings.Contains(arg, ":")) &&
+			!strings.Contains(arg, "@") &&
+			!strings.Contains(arg, "/") &&
+			!strings.HasPrefix(arg, "-") &&
+			!strings.HasPrefix(arg, "+") {
 			opts.Name = arg
 		}
 	}
