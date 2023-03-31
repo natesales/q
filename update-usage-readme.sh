6c7d9d946d916c441e2afd40506ccd89c4708057
Nate Sales <nate@natesales.net>
2023-03-31 00:29:57 -0400

    fix: try to not split lines

--- update-usage-readme.sh
+++ update-usage-readme.sh
@@ -2,7 +2,7 @@
 
 cat README.md | sed '/# Usage/q' > README.tmp.md
 echo '```' >> README.tmp.md
-go build && ./q -h | sed -z '$ s/\n$//' >> README.tmp.md
+go build && ./q -h | cat | sed -z '$ s/\n$//' >> README.tmp.md
 printf '```\n\n### Demo\n' >> README.tmp.md
 cat README.md | sed '1,/### Demo/d' >> README.tmp.md
 mv README.tmp.md README.md
