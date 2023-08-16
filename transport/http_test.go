b0557ddea798c35a39795fbc17a6a26f3a65b9f4
Nate Sales <nate@natesales.net>
2023-08-16 01:52:52 -0700

    fix: use high port in tests

--- transport/http_test.go
+++ transport/http_test.go
@@ -39,22 +39,24 @@ func TestTransportHTTPInvalidResolver(t *testing.T) {
 }
 
 func TestTransportHTTPServerError(t *testing.T) {
+	listen := ":53807"
 	go func() {
-		http.ListenAndServe(":8084", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ListenAndServe(listen, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Server Error", http.StatusInternalServerError)
 		}))
 	}()
 
 	tp := httpTransport()
-	tp.Server = "http://localhost:8084"
+	tp.Server = "http://localhost" + listen
 	_, err := tp.Exchange(validQuery())
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "got status code 500")
 }
 
 func TestTransportHTTPIDMismatch(t *testing.T) {
+	listen := ":53808"
 	go func() {
-		http.ListenAndServe(":8085", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ListenAndServe(listen, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			msg := dns.Msg{}
 			msg.Id = 1
 			buf, err := msg.Pack()
@@ -67,7 +69,7 @@ func TestTransportHTTPIDMismatch(t *testing.T) {
 	}()
 
 	tp := httpTransport()
-	tp.Server = "http://localhost:8085"
+	tp.Server = "http://localhost" + listen
 	query := validQuery()
 	reply, err := tp.Exchange(query)
 	assert.Nil(t, err)
