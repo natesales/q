0f1513d03514e1604c19670cc7ed7be896d1ec98
Nate Sales <nate@natesales.net>
2023-10-20 03:50:01 -0400

    fix: don't wrap usage text in readme

--- update-usage-readme.sh
+++ update-usage-readme.sh
@@ -2,7 +2,8 @@
 
 cat README.md | sed '/# Usage/q' > README.tmp.md
 printf '\n```text\n' >> README.tmp.md
-go build && ./q -h | cat | sed -z '$ s/\n$//' >> README.tmp.md
+stty rows 200 cols 200
+go build && ./q -h | sed -z '$ s/\n$//' >> README.tmp.md
 printf '```\n\n### Demo\n' >> README.tmp.md
 cat README.md | sed '1,/### Demo/d' >> README.tmp.md
 mv README.tmp.md README.md
