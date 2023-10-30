3df3120aca6da2b45fb6949b97130454716c0e50
Nate Sales <nate@natesales.net>
2023-10-29 20:17:25 -0400

    test: pretty column format output

---
+++ output/output_test.go
@@ -0,0 +1,33 @@
+package output
+
+import (
+	"strings"
+
+	"github.com/miekg/dns"
+)
+
+// replies returns a slice of example DNS answer messages
+func replies() []*dns.Msg {
+	testZone := `
+example.com. 86400 IN A 192.0.2.1
+example.com. 86400 IN A 192.0.2.2
+example.com. 86400 IN NS b.iana-servers.net.
+example.com. 86400 IN NS a.iana-servers.net.
+example.com. 86400 IN MX 0 .
+example.com. 86400 IN TXT "v=spf1 -all"
+`
+
+	// Convert the zone to DNS messages
+	var msgs []*dns.Msg
+	for _, line := range strings.Split(testZone, "\n") {
+		if line != "" {
+			rr, err := dns.NewRR(line)
+			if err != nil {
+				panic(err)
+			}
+			msgs = append(msgs, &dns.Msg{Answer: []dns.RR{rr}})
+		}
+	}
+
+	return msgs
+}
--- output/pretty_test.go
+++ output/pretty_test.go
@@ -1,9 +1,13 @@
 package output
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/natesales/q/cli"
+	"github.com/natesales/q/util"
 )
 
 func TestOutputPrettyPTRCache(t *testing.T) {
@@ -15,4 +19,16 @@ func TestOutputPrettyPTRCache(t *testing.T) {
 	ptr, err := p.ptr("192.0.2.1")
 	assert.Nil(t, err)
 	assert.Equal(t, "example.com.", ptr)
+}
+
+func TestOutputPrettyPrintColumn(t *testing.T) {
+	var buf bytes.Buffer
+	util.UseColor = false
+	p := Printer{Out: &buf, Opts: &cli.Flags{Format: "column"}}
+	p.PrintColumn(replies())
+	assert.Contains(t, buf.String(), `A 86400 192.0.2.2`)
+	assert.Contains(t, buf.String(), `MX 86400 0 .`)
+	assert.Contains(t, buf.String(), `NS 86400 a.iana-servers.net.`)
+	assert.Contains(t, buf.String(), `NS 86400 b.iana-servers.net.`)
+	assert.Contains(t, buf.String(), `TXT 86400 "v=spf1 -all"`)
 }
