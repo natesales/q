0f1a98f0a4de57fbbce451c1379c69a031a5e284
Nate Sales <nate@natesales.net>
2023-12-12 23:56:20 -0500

    fix: no readme wrap

--- update-usage-readme.sh
+++ update-usage-readme.sh
@@ -2,7 +2,7 @@
 
 cat README.md | sed '/# Usage/q' > README.tmp.md
 printf '\n```text\n' >> README.tmp.md
-stty rows 200 cols 200
+stty rows 1000 cols 1000
 go build && ./q -h | sed -z '$ s/\n$//' >> README.tmp.md
 printf '```\n\n### Demo\n' >> README.tmp.md
 cat README.md | sed '1,/### Demo/d' >> README.tmp.md
