37acb1e19e03f1f6704a4e392d3af6f797d873b5
Nate Sales <nate@natesales.net>
2023-12-12 23:41:30 -0500

    refactor: cleanup main test harness

--- main_test.go
+++ main_test.go
@@ -14,387 +14,356 @@ import (
 	"github.com/natesales/q/transport"
 )
 
-func TestMainQuery(t *testing.T) {
+func run(args ...string) (*bytes.Buffer, error) {
 	clearOpts()
 	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	err := driver(append([]string{"+nocolor"}, args...), &out)
+	return &out, err
+}
+
+func TestMainQuery(t *testing.T) {
+	out, err := run(
 		"--all",
 		"-q", "example.com",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* TXT "v=spf1 -all"`), out.String())
 }
 
 func TestMainVersion(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
-		"-V",
-	}, &out))
+	out, err := run("-V")
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), "https://github.com/natesales/q version dev (unknown unknown)")
 }
 
 // TODO
-// func TestMainODoHQuery(t *testing.T) {
-// 	clearOpts()
-// 	assert.Nil(t, driver([]string{
-// 		"--all",
-// 		"-q", "example.com",
-// 		"-s", "https://odoh.cloudflare-dns.com",
-// 		"--odoh-proxy", "https://odoh.crypto.sx",
-// 	}, &out))
-// }
+//func TestMainODoHQuery(t *testing.T) {
+//	out, err := run(
+//		"--all",
+//		"-q", "example.com",
+//		"-s", "https://odoh.cloudflare-dns.com",
+//		"--odoh-proxy", "https://odoh.crypto.sx",
+//	)
+//	assert.Nil(t, err)
+//	assert.Regexp(t, regexp.MustCompile(`example.com. .* TXT "v=spf1 -all"`), out.String())
+//}
 
 func TestMainRawFormat(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"--format=raw",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), "v=spf1 -all")
 	assert.Contains(t, out.String(), "a.iana-servers.net")
 }
 
 func TestMainJSONFormat(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"--format=json",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), `"Preference":0,"Mx":"."`)
 	assert.Contains(t, out.String(), `"Ns":"a.iana-servers.net."`)
 	assert.Contains(t, out.String(), `"Txt":["v=spf1 -all"]`)
 }
 
 func TestMainInvalidOutputFormat(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	err := driver([]string{
+	_, err := run(
 		"--all",
 		"-q", "example.com",
 		"--format=invalid",
-	}, &out)
+	)
 	if !(err != nil && strings.Contains(err.Error(), "invalid output format")) {
 		t.Errorf("invalid output format should throw an error")
 	}
 }
 
 func TestMainParseTypes(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"-t", "A",
 		"-t", "AAAA",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* AAAA .*`), out.String())
 }
 
 func TestMainInvalidTypes(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	err := driver([]string{
+	_, err := run(
 		"--all",
 		"-q", "example.com",
 		"-t", "INVALID",
-	}, &out)
+	)
 	if !(err != nil && strings.Contains(err.Error(), "INVALID is not a valid RR type")) {
 		t.Errorf("expected invalid type error, got %+v", err)
 	}
 }
 
 func TestMainInvalidODoHUpstream(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	err := driver([]string{
+	_, err := run(
 		"--all",
 		"-q", "example.com",
 		"-s", "tls://odoh.cloudflare-dns.com",
 		"--odoh-proxy", "https://odoh.crypto.sx",
-	}, &out)
+	)
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "ODoH target must use HTTPS")
 }
 
 func TestMainInvalidODoHProxy(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	err := driver([]string{
+	_, err := run(
 		"--all",
 		"-q", "example.com",
 		"-s", "https://odoh.cloudflare-dns.com",
 		"--odoh-proxy", "tls://odoh1.surfdomeinen.nl",
-	}, &out)
+	)
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "ODoH proxy must use HTTPS")
 }
 
 func TestMainReverseQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-x",
 		"-q", "1.1.1.1",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`1.1.1.1.in-addr.arpa. .* PTR one.one.one.one`), out.String())
 }
 
 func TestMainInferredQname(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"example.com",
 		"A",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainInferredServer(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"@8.8.8.8",
 		"-t", "A",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainInvalidReverseQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	err := driver([]string{
+	_, err := run(
 		"--all",
 		"-x",
 		"example.com",
-	}, &out)
+	)
 	if !(err != nil && strings.Contains(err.Error(), "unrecognized address: example.com")) {
 		t.Errorf("expected address error, got %+v", err)
 	}
 }
 
 func TestMainInvalidUpstream(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	err := driver([]string{
+	_, err := run(
 		"--all",
 		"-s", "127.127.127.127:1",
 		"example.com",
-	}, &out)
+	)
 	if !(err != nil && strings.Contains(err.Error(), "connection refused")) {
 		t.Errorf("expected connection error, got %+v", err)
 	}
 }
 
 func TestMainDNSSECArg(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"example.com",
 		"+dnssec",
 		"@9.9.9.9",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	t.Logf("out: %s", out.String())
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* RRSIG .*`), out.String())
 }
 
 func TestMainPad(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"--pad",
 		"--format=json",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), `"Truncated":false`)
 }
 
 func TestMainChaosClass(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"id.server",
 		"CH",
 		"TXT",
 		"@9.9.9.9",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`id.server. .* TXT ".*.pch.net"`), out.String())
 }
 
 func TestMainParsePlusFlags(t *testing.T) {
-	clearOpts()
 	cli.ParsePlusFlags(&opts, []string{"+dnssec", "+nord"})
 	assert.True(t, opts.DNSSEC)
 	assert.False(t, opts.RecursionDesired)
 }
 
 func TestMainTCPQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-t", "A",
 		"-q", "example.com",
 		"@tcp://1.1.1.1",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainTLSQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"-t", "A",
 		"@tls://1.1.1.1",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainHTTPSQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"-t", "A",
 		"@https://dns.quad9.net",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainQUICQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"-t", "A",
 		"@quic://dns.adguard.com",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainDNSCryptStampQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"-q", "example.com",
 		"-t", "A",
 		"@sdns://AQMAAAAAAAAAETk0LjE0MC4xNC4xNDo1NDQzINErR_JS3PLCu_iZEIbq95zkSV2LFsigxDIuUso_OQhzIjIuZG5zY3J5cHQuZGVmYXVsdC5uczEuYWRndWFyZC5jb20",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainDNSCryptManualQuery(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"-q", "example.com",
 		"-t", "A",
 		"@dnscrypt://94.140.14.14:5443",
 		"--dnscrypt-key", "d12b47f252dcf2c2bbf8991086eaf79ce4495d8b16c8a0c4322e52ca3f390873",
 		"--dnscrypt-provider", "2.dnscrypt.default.ns1.adguard.com",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainInvalidServerURL(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.NotNil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"@bad::server::url",
 		"--format=json",
-	}, &out))
+	)
+	assert.NotNil(t, err)
 	assert.NotRegexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainInvalidTransportScheme(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.NotNil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"@invalid://example.com",
 		"--format=json",
-	}, &out))
+	)
+	assert.NotNil(t, err)
 	assert.NotRegexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainTLS12(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"-q", "example.com",
 		"--tls-min-version=1.1",
 		"--tls-max-version=1.2",
 		"@tls://dns.quad9.net",
 		"-t", "A",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`example.com. .* A .*`), out.String())
 }
 
 func TestMainNSID(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"@9.9.9.9",
 		"+nsid",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`.*.pch.net.*`), out.String())
 }
 
 func TestMainECSv4(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"@script-ns.packetframe.com",
 		"TXT",
 		"query.script.packetframe.com",
 		"--subnet", "192.0.2.0/24",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), `'subnet':'192.0.2.0/24/0'`)
 }
 
 func TestMainECSv6(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"@script-ns.packetframe.com",
 		"TXT",
 		"query.script.packetframe.com",
 		"--subnet", "2001:db8::/48",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), `'subnet':'[2001:db8::]/48/0'`)
 }
 
 func TestMainHTTPUserAgent(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"@https://dns.quad9.net",
 		"--http-user-agent", "Example/1.0",
 		"-t", "NS",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`. .* NS a.root-servers.net.`), out.String())
 }
 
@@ -505,13 +474,12 @@ func TestMainParseServer(t *testing.T) {
 }
 
 func TestMainRecAXFR(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"--all",
 		"+recaxfr",
 		"@nsztm1.digi.ninja", "zonetransfer.me",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), `AXFR zonetransfer.me.`)
 	assert.Contains(t, out.String(), `AXFR internal.zonetransfer.me.`)
 
@@ -524,15 +492,14 @@ func TestMainRecAXFR(t *testing.T) {
 }
 
 func TestMainShowAll(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"@9.9.9.9",
 		"--all",
 		"+all",
 		"example.com",
 		"A",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Contains(t, out.String(), "example.com.")
 	assert.Contains(t, out.String(), "Question:")
 	assert.Contains(t, out.String(), "Answer:")
@@ -540,12 +507,11 @@ func TestMainShowAll(t *testing.T) {
 }
 
 func TestMainResolveIPs(t *testing.T) {
-	clearOpts()
-	var out bytes.Buffer
-	assert.Nil(t, driver([]string{
+	out, err := run(
 		"core1.fmt2.he.net",
 		"A", "AAAA",
 		"-R",
-	}, &out))
+	)
+	assert.Nil(t, err)
 	assert.Regexp(t, regexp.MustCompile(`core1.fmt2.he.net. .* A .* \(core1.fmt2.he.net.\)`), out.String())
 }
